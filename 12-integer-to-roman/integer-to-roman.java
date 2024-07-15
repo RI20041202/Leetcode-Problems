85b9c09bb5631d08e546aba4be51e4a2049a2793
rishita alluri <141256911+RI20041202@users.noreply.github.com>
2024-07-15 14:34:45 +0530

    Time: 8 ms (27.71%) | Memory: 44.9 MB (11.91%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String intToRoman(int num) {
+       LinkedHashMap<Integer, String> roman = new LinkedHashMap<>();
+        roman.put(1000, "M");
+        roman.put(900, "CM");
+        roman.put(500, "D");
+        roman.put(400, "CD");
+        roman.put(100, "C");
+        roman.put(90, "XC");
+        roman.put(50, "L");
+        roman.put(40, "XL");
+        roman.put(10, "X");
+        roman.put(9, "IX");
+        roman.put(5, "V");
+        roman.put(4, "IV");
+        roman.put(1, "I");
+        StringBuilder st = new StringBuilder();
+        for (Map.Entry<Integer, String> entry : roman.entrySet()) {
+            while (num >= entry.getKey()) {
+                st.append(entry.getValue());
+                num -= entry.getKey();
+            }
+        }
+        
+        return st.toString();
+    }
+}
